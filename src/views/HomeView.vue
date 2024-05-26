cf4501dae30b20708782efc6921b3ac43f4f8224
lmk123568 <lmk123568@qq.com>
2024-05-27 00:50:59 +0800

    [5/26] 优化主页排版

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { NButton } from 'naive-ui'
-import { useRouter } from 'vue-router';
+import {NButton} from 'naive-ui'
+import {useRouter} from 'vue-router';
 
 const router = useRouter()
 const toResume = () => {
@@ -23,24 +23,18 @@ const toResume = () => {
       <li></li>
       <li></li>
     </ul>
-    <div class="row">
-      <div class="col-md-4"></div>
-      <div class="col-md-4">
-        <div class="vh-100 d-flex flex-column justify-content-center align-items-center">
 
-          <img src="../assets/avatar.jpg" class="rounded-circle" alt="头像" width="128"/>
+    <div class="vh-100 d-flex flex-column justify-content-center align-items-center">
 
-          <p class="fs-1 fw-bold my-3 text-white">Hello there 👋</p>
-          <p class="fs-6 mb-2 text-white">我是 Mike，毕业于 UCAS，我是一名听障人士 🦻🏼</p>
-          <p class="fs-6 text-white">得益于编程 👨‍💻，我能巧妙避免听力劣势从而实现自我价值</p>
-          <n-button type="primary" @click="toResume" class="rounded-5 my-2">
-            认识一下我
-          </n-button>
+      <img alt="头像" class="rounded-circle" src="../assets/avatar.jpg" width="128"/>
 
-        </div>
+      <p class="fs-1 fw-bold my-3 text-white">Hello there 👋</p>
+      <p class="fs-6 mb-2 text-white">我是 Mike，毕业于 UCAS，我是一名听障人士 🦻🏼</p>
+      <p class="fs-6 text-white">得益于编程 👨‍💻，我能巧妙避免听力劣势从而实现自我价值</p>
+      <n-button class="rounded-5 my-2" type="primary" @click="toResume">
+        认识一下我
+      </n-button>
 
-      </div>
-      <div class="col-md-4"></div>
     </div>
 
 
@@ -51,15 +45,15 @@ const toResume = () => {
 
 <style scoped>
 
-.dynamic-bg{
+.dynamic-bg {
   background: #4e54c8;
   background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);
   width: 100%;
-  height:100vh;
+  height: 100vh;
 
 }
 
-.circles{
+.circles {
   position: absolute;
   top: 0;
   left: 0;
@@ -68,7 +62,7 @@ const toResume = () => {
   overflow: hidden;
 }
 
-.circles li{
+.circles li {
   position: absolute;
   display: block;
   list-style: none;
@@ -80,7 +74,7 @@ const toResume = () => {
 
 }
 
-.circles li:nth-child(1){
+.circles li:nth-child(1) {
   left: 25%;
   width: 80px;
   height: 80px;
@@ -88,7 +82,7 @@ const toResume = () => {
 }
 
 
-.circles li:nth-child(2){
+.circles li:nth-child(2) {
   left: 10%;
   width: 20px;
   height: 20px;
@@ -96,14 +90,14 @@ const toResume = () => {
   animation-duration: 12s;
 }
 
-.circles li:nth-child(3){
+.circles li:nth-child(3) {
   left: 70%;
   width: 20px;
   height: 20px;
   animation-delay: 4s;
 }
 
-.circles li:nth-child(4){
+.circles li:nth-child(4) {
   left: 40%;
   width: 60px;
   height: 60px;
@@ -111,28 +105,28 @@ const toResume = () => {
   animation-duration: 18s;
 }
 
-.circles li:nth-child(5){
+.circles li:nth-child(5) {
   left: 65%;
   width: 20px;
   height: 20px;
   animation-delay: 0s;
 }
 
-.circles li:nth-child(6){
+.circles li:nth-child(6) {
   left: 75%;
   width: 110px;
   height: 110px;
   animation-delay: 3s;
 }
 
-.circles li:nth-child(7){
+.circles li:nth-child(7) {
   left: 35%;
   width: 150px;
   height: 150px;
   animation-delay: 7s;
 }
 
-.circles li:nth-child(8){
+.circles li:nth-child(8) {
   left: 50%;
   width: 25px;
   height: 25px;
@@ -140,7 +134,7 @@ const toResume = () => {
   animation-duration: 45s;
 }
 
-.circles li:nth-child(9){
+.circles li:nth-child(9) {
   left: 20%;
   width: 15px;
   height: 15px;
@@ -148,7 +142,7 @@ const toResume = () => {
   animation-duration: 35s;
 }
 
-.circles li:nth-child(10){
+.circles li:nth-child(10) {
   left: 85%;
   width: 150px;
   height: 150px;
@@ -157,16 +151,15 @@ const toResume = () => {
 }
 
 
-
 @keyframes animate {
 
-  0%{
+  0% {
     transform: translateY(0) rotate(0deg);
     opacity: 1;
     border-radius: 0;
   }
 
-  100%{
+  100% {
     transform: translateY(-1000px) rotate(720deg);
     opacity: 0;
     border-radius: 50%;
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -7,7 +7,7 @@
       <div class="row">
         <div class="col-md-3 vh-100 d-flex flex-column align-items-center">
 
-          <img src="../assets/avatar.jpg" class="rounded-circle mt-5 mb-3" alt="头像" width="128"/>
+          <img alt="头像" class="rounded-circle mt-5 mb-3" src="../assets/avatar.jpg" width="128"/>
 
           <p class="fs-4 fw-bold my-2">Mike Liu</p>
           <p class="fs-6 my-1">中国科学院大学</p>
@@ -19,11 +19,11 @@
           <p class="bi bi-mortarboard-fill my-2"> Google Scholar</p>
 
 
-
         </div>
         <div class="col-md-9">
           <p class="fs-4 fw-bold mt-5">🤗 简介</p>
-          <p class="fs-6">我硕士毕业于中国科学院大学的微电子所（京所），师从王云教授。我目前在国资委所属央国企研发部门担任 AI 算法研究员，职级是高级工程师。</p>
+          <p class="fs-6">我硕士毕业于中国科学院大学的微电子所（京所），师从王云教授。我目前在国资委所属央国企研发部门担任
+            AI 算法研究员，职级是高级工程师。</p>
           <p class="fs-6">我的研究领域包括：</p>
           <ul>
             <li>Object Det、Seg、Track</li>
@@ -32,8 +32,12 @@
 
           <p class="fs-4 fw-bold">🎓 学历</p>
           <ul>
-            <li>2019 - 2022,<img src="../assets/ucas_logo.png" width="18px" class="mx-2">中国科学院大学 - 计算机技术 - 硕士</li>
-            <li class="mt-1">2015 - 2019,<img src="../assets/xiyou_logo.jpg" width="18px" class="mx-2">西安邮电大学 - 电子信息科学与技术 - 本科</li>
+            <li>2019 - 2022,<img class="mx-2" src="../assets/ucas_logo.png" width="18px">中国科学院大学 - 计算机技术 -
+              硕士
+            </li>
+            <li class="mt-1">2015 - 2019,<img class="mx-2" src="../assets/xiyou_logo.jpg" width="18px">西安邮电大学 -
+              电子信息科学与技术 - 本科
+            </li>
           </ul>
 
           <p class="fs-4 fw-bold">📝 论文专利</p>
